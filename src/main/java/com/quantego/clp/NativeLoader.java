59e4b93f1fe6d5634af485e25af82c919d554f52
loehndorf <nils@loehndorf.com>
2022-01-06 22:13:57 +0100

    added aarch64 support

--- src/main/java/com/quantego/clp/NativeLoader.java
+++ src/main/java/com/quantego/clp/NativeLoader.java
@@ -8,40 +8,49 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 class NativeLoader {
-	
+
 	static String prefix = "CLPExtractedLib";
 	static String library = "clp-1.16.11";
 	static String pathSep = System.getProperty("file.separator");
-	
+
 	public static void main(String... args) {
-		load();
-		CLPNative.clpNewModel();
+		System.err.println(load());
 	}
 
-	static void load() {
+	static String load() {
 		File tempDir = createTempDir(prefix);
-        BridJ.addLibraryPath(tempDir.getAbsolutePath());
+		BridJ.addLibraryPath(tempDir.getAbsolutePath());
 		String osArch = System.getProperty("os.arch");
-        String osName = System.getProperty("os.name").toLowerCase();
-        String path;
-        String[] libs;
-        if (osName.startsWith("mac")) {
-        	path = library+"/darwin/";
-        	libs = new String[]{"libCoinUtils.3.dylib","libClp.dylib"};
-        } else if (osName.startsWith("win") && osArch.contains("64")) {
-        	path = library+"/win64/";
-            libs = new String[]{"libgcc_s_seh-1.dll","libstdc++-6.dll","libCoinUtils-3.dll","Clp.dll"};
-        } else if (osName.startsWith("linux")) {
-        	path = library+"/linux64/";
-        	libs = new String[]{"libCoinUtils.so.3","libClp.so"};
-            BridJ.addNativeLibraryDependencies("Clp", "CoinUtils");
-        } else {
-            throw new UnsupportedOperationException("Platform " + osName + ":" + osArch + " not supported");
-        }
-        for (String lib : libs) 
-    		loadLibrary(tempDir,path,lib);
+	    String osName = System.getProperty("os.name").toLowerCase();
+	    String path;
+	    String[] libs;
+	    String selectedArch = null;
+		if (osArch.equalsIgnoreCase("aarch64")) {
+			path = library + "/aarch64/";
+			libs = new String[]{"libCoinUtils.so.3", "libClp.so"};
+			BridJ.addNativeLibraryDependencies("Clp", "CoinUtils");
+			selectedArch = "mac-aarch64";
+	    } else if (osName.startsWith("mac")) {
+	    	path = library+"/darwin/";
+	    	libs = new String[]{"libCoinUtils.3.dylib","libClp.dylib"};
+	    	selectedArch = "mac-x86_64";
+	    } else if (osName.startsWith("win") && osArch.contains("64")) {
+	    	path = library+"/win64/";
+	        libs = new String[]{"libgcc_s_seh-1.dll","libstdc++-6.dll","libCoinUtils-3.dll","Clp.dll"};
+	        selectedArch = "win-x86_64";
+	    } else if (osName.startsWith("linux")) {
+	    	path = library+"/linux64/";
+	    	libs = new String[]{"libCoinUtils.so.3","libClp.so"};
+	        BridJ.addNativeLibraryDependencies("Clp", "CoinUtils");
+	        selectedArch = "linux-x86_64";
+	    } else {
+	        throw new UnsupportedOperationException("Platform " + osName + ":" + osArch + " not supported");
+	    }
+	    for (String lib : libs)
+			loadLibrary(tempDir,path,lib);
+	    return String.format("Loaded libraries: %s, architecture: %s, system: %s.", selectedArch, osArch, osName);
 	}
-	
+
 	public static File createTempDir(String prefix) {
         String tmpDirName = System.getProperty("java.io.tmpdir");
         if (tmpDirName.endsWith(pathSep))
@@ -53,7 +62,7 @@ class NativeLoader {
         dir.deleteOnExit();
         return dir;
 	}
-	
+
     public static void loadLibrary(File dir, String path, String name) {
         InputStream in = null;
         OutputStream out = null;
@@ -85,5 +94,5 @@ class NativeLoader {
             }
         }
     }
-    
+
 }
